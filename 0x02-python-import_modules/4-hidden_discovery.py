1580aa46c405d63c710f59a72dd1f90deb94cc87
Abenieph <abenezerephrem03111994@gmail.com>
2022-06-02 13:57:40 -0700

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-import hidden_4
-
-
-def principal():
-    for i in dir(hidden_4):
-        if not (i[0] == '_' and i[1] == '_'):
-            print(i)
 
 if __name__ == "__main__":
-    principal()
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
