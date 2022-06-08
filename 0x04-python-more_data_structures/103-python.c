685cf77981d29204876085f8a71ab18c89b093bd
Abenieph <abenezerephrem03111994@gmail.com>
2022-06-08 13:11:19 -0700

    103 advanced

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,112 +1,66 @@
-import ctypes
+#include <Python.h>
 
-lib = ctypes.CDLL('./libPython.so')
-lib.print_python_list.argtypes = [ctypes.py_object]
-lib.print_python_bytes.argtypes = [ctypes.py_object]
-s = b"Hello"
-lib.print_python_bytes(s);
-b = b'\xff\xf8\x00\x00\x00\x00\x00\x00';
-lib.print_python_bytes(b);
-b = b'What does the \'b\' character do in front of a string literal?';
-lib.print_python_bytes(b);
-l = [b'Hello', b'World']
-lib.print_python_list(l)
-del l[1]
-lib.print_python_list(l)
-l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], b"Holberton", "Betty"]
-lib.print_python_list(l)
-l = []
-lib.print_python_list(l)
-l.append(0)
-lib.print_python_list(l)
-l.append(1)
-l.append(2)
-l.append(3)
-l.append(4)
-lib.print_python_list(l)
-l.pop()
-lib.print_python_list(l)
-l = ["Holberton"]
-lib.print_python_list(l)
-lib.print_python_bytes(l);
-julien@ubuntu:~/CPython$ python3 103-tests.py 
-[.] bytes object info
-  size: 5
-  trying string: Hello
-  first 6 bytes: 48 65 6c 6c 6f 00
-[.] bytes object info
-  size: 8
-  trying string: �
-  first 9 bytes: ff f8 00 00 00 00 00 00 00
-[.] bytes object info
-  size: 60
-  trying string: What does the 'b' character do in front of a string literal?
-  first 10 bytes: 57 68 61 74 20 64 6f 65 73 20
-[*] Python list info
-[*] Size of the Python List = 2
-[*] Allocated = 2
-Element 0: bytes
-[.] bytes object info
-  size: 5
-  trying string: Hello
-  first 6 bytes: 48 65 6c 6c 6f 00
-Element 1: bytes
-[.] bytes object info
-  size: 5
-  trying string: World
-  first 6 bytes: 57 6f 72 6c 64 00
-[*] Python list info
-[*] Size of the Python List = 1
-[*] Allocated = 2
-Element 0: bytes
-[.] bytes object info
-  size: 5
-  trying string: Hello
-  first 6 bytes: 48 65 6c 6c 6f 00
-[*] Python list info
-[*] Size of the Python List = 8
-[*] Allocated = 8
-Element 0: bytes
-[.] bytes object info
-  size: 5
-  trying string: Hello
-  first 6 bytes: 48 65 6c 6c 6f 00
-Element 1: int
-Element 2: int
-Element 3: float
-Element 4: tuple
-Element 5: list
-Element 6: bytes
-[.] bytes object info
-  size: 9
-  trying string: Holberton
-  first 10 bytes: 48 6f 6c 62 65 72 74 6f 6e 00
-Element 7: str
-[*] Python list info
-[*] Size of the Python List = 0
-[*] Allocated = 0
-[*] Python list info
-[*] Size of the Python List = 1
-[*] Allocated = 4
-Element 0: int
-[*] Python list info
-[*] Size of the Python List = 5
-[*] Allocated = 8
-Element 0: int
-Element 1: int
-Element 2: int
-Element 3: int
-Element 4: int
-[*] Python list info
-[*] Size of the Python List = 4
-[*] Allocated = 8
-Element 0: int
-Element 1: int
-Element 2: int
-Element 3: int
-[*] Python list info
-[*] Size of the Python List = 1
-[*] Allocated = 1
-Element 0: str
-[.] bytes object info
-  [ERROR] Invalid Bytes Object
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+
+/**
+ * print_python_list - Prints basic info about Python lists.
+ * @p: A PyObject list object.
+ */
+void print_python_list(PyObject *p)
+{
+  int size, alloc, i;
+  const char *type;
+  PyListObject *list = (PyListObject *)p;
+  PyVarObject *var = (PyVarObject *)p;
+
+  size = var->ob_size;
+  alloc = list->allocated;
+
+  printf("[*] Python list info\n");
+  printf("[*] Size of the Python List = %d\n", size);
+  printf("[*] Allocated = %d\n", alloc);
+
+  for (i = 0; i < size; i++)
+    {
+      type = list->ob_item[i]->ob_type->tp_name;
+      printf("Element %d: %s\n", i, type);
+      if (strcmp(type, "bytes") == 0)
+	print_python_bytes(list->ob_item[i]);
+    }
+}
+
+/**
+ * print_python_bytes - Prints basic info about Python byte objects.
+ * @p: A PyObject byte object.
+ */
+void print_python_bytes(PyObject *p)
+{
+  unsigned char i, size;
+  PyBytesObject *bytes = (PyBytesObject *)p;
+
+  printf("[.] bytes object info\n");
+  if (strcmp(p->ob_type->tp_name, "bytes") != 0)
+    {
+      printf("  [ERROR] Invalid Bytes Object\n");
+      return;
+    }
+
+  printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
+  printf("  trying string: %s\n", bytes->ob_sval);
+
+  if (((PyVarObject *)p)->ob_size > 10)
+    size = 10;
+  else
+    size = ((PyVarObject *)p)->ob_size + 1;
+
+  printf("  first %d bytes: ", size);
+  for (i = 0; i < size; i++)
+    {
+      printf("%02hhx", bytes->ob_sval[i]);
+      if (i == (size - 1))
+	printf("\n");
+      else
+	printf(" ");
+    }
+}
