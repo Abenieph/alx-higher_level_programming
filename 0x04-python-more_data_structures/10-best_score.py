3d4ca5abf8ed344a824d5992a1bda569e5abcced
Abenieph <abenezerephrem03111994@gmail.com>
2022-06-08 12:32:49 -0700

    added function that returns a key with the biggest integer value.

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+10-best_score.py
+[A[C[C[C[C[C[C[C[C[C[C[#!/usr/bin/python3
+def best_score(my_dict):
+    if my_dict and len(my_dict):
+        max = list(my_dict.keys())[0]
+        for key in my_dict:
+            if my_dict[key] > my_dict[max]:
+                max = key
+        return max
+    return None
